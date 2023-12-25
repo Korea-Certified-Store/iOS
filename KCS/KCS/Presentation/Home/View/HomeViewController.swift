5b7df7a2bbd216fec486ae1091818fb0c54d23ea
SungMinCho-Kor <rhxoeh0011@gmail.com>
2023-12-25 12:18:43 +0900

    feat : Map View에 추가 및 layout (#19)

---
+++ KCS/KCS/Presentation/Home/View/HomeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  KCS
+//
+//  Created by 조성민 on 12/24/23.
+//
+
+import UIKit
+import NMapsMap
+
+final class HomeViewController: UIViewController {
+
+    private let mapView: NMFMapView = {
+        let map = NMFMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        
+        return map
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addUIComponents()
+        configureConstraints()
+    }
+    
+    private func addUIComponents() {
+        view.addSubview(mapView)
+    }
+    
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
+
+}
