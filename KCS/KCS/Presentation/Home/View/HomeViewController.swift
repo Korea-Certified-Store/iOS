2c11c331252c391ec0864331ee2cdeb3c29ead26
k2645 <k2645@naver.com>
2024-01-01 15:43:51 +0900

    feat : 위치 권한 확인 코드 작성 중.. (#23)

--- KCS/KCS/Presentation/Home/View/HomeViewController.swift
+++ KCS/KCS/Presentation/Home/View/HomeViewController.swift
@@ -7,8 +7,16 @@
 
 import UIKit
 import NMapsMap
+import CoreLocation
 
 final class HomeViewController: UIViewController {
+    
+    private lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.delegate = self
+        
+        return manager
+    }()
 
     private let mapView: NMFNaverMapView = {
         let map = NMFNaverMapView()
@@ -23,6 +31,7 @@ final class HomeViewController: UIViewController {
         
         addUIComponents()
         configureConstraints()
+        checkUserDeviceLocationServiceAuthorization()
     }
 
 }
@@ -40,6 +49,64 @@ private extension HomeViewController {
             mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
             mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
         ])
+    }
+    
+}
+
+extension HomeViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coordinate = locations.last?.coordinate {
+            print(coordinate)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkUserDeviceLocationServiceAuthorization()
+    }
+    
+    func checkUserDeviceLocationServiceAuthorization() {
+        guard CLLocationManager.locationServicesEnabled() else {
+            showRequestLocationServiceAlert()
+            
+            return
+        }
+        let authorizationStatus: CLAuthorizationStatus = locationManager.authorizationStatus
+        checkUserCurrentLocationAuthorization(authorizationStatus)
+    }
+    
+    func checkUserCurrentLocationAuthorization(_ status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse:
+            locationManager.requestLocation()
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            showRequestLocationServiceAlert()
+        default:
+            print("defalut")
+        }
+    }
+    
+    func showRequestLocationServiceAlert() {
+        let requestLocationServiceAlert = UIAlertController(title: "위치 정보 이용", message: "위치 서비스를 사용할 수 없습니다.\n디바이스의 '설정 > 개인정보 보호'에서 위치 서비스를 켜주세요.", preferredStyle: .alert)
+        let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+            if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(appSetting)
+            }
+        }
+        let cancel = UIAlertAction(title: "취소", style: .default) { _ in
+            print("취소")
+        }
+        
+        requestLocationServiceAlert.addAction(cancel)
+        requestLocationServiceAlert.addAction(goSetting)
+        
+        present(requestLocationServiceAlert, animated: true)
     }
     
 }
