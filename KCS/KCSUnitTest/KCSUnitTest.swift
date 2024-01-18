518da45a3c9832679ced03aefddab83602be31d7
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-01-18 23:05:18 +0900

    test : test 환경 설정 (#60)

---
+++ KCS/KCSUnitTest/KCSUnitTest.swift
@@ -0,0 +1,23 @@
+//
+//  StoreRepositoryImplTests.swift
+//  StoreRepositoryImplTests
+//
+//  Created by 조성민 on 1/18/24.
+//
+
+import XCTest
+@testable import KCS
+
+final class StoreRepositoryImplTests: XCTestCase {
+
+    var storeRepositoryImpl: StoreRepositoryImpl?
+    
+    override func setUp() {
+        storeRepositoryImpl = StoreRepositoryImpl()
+    }
+
+    func testExample() {
+        
+    }
+
+}
