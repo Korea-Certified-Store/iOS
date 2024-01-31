e40ebc8745eca97c7866712acb373a566a9a8635
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-01-31 17:21:16 +0900

    chore : StoreViewController 생성 (#139)

---
+++ KCS/KCS/Presentation/StoreList/StoreListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StoreListViewController.swift
+//  KCS
+//
+//  Created by 조성민 on 1/31/24.
+//
+
+import UIKit
+
+final class StoreListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
