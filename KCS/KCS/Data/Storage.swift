8f20b9c1d6822b476fbfc417db80ebeabd4bd18b
k2645 <k2645@naver.com>
2024-02-05 14:15:17 +0900

    feat : 앱 최초 실행 확인을 위한 UserDefault 추가 (#167)

---
+++ KCS/KCS/Data/Storage.swift
@@ -0,0 +1,22 @@
+//
+//  Storage.swift
+//  KCS
+//
+//  Created by 김영현 on 2/5/24.
+//
+
+import Foundation
+
+final class Storage {
+    
+    static func isFirstTime() -> Bool {
+        let defaults = UserDefaults.standard
+        if defaults.object(forKey: "isFirstTime") == nil {
+            defaults.set(false, forKey:"isFirstTime")
+            return true
+        } else {
+            return false
+        }
+    }
+    
+}
