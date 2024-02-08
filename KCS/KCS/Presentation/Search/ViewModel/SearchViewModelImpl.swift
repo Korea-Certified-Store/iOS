770866ec6b2a3904fce92e95c9b538eba5f7c1c4
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-02-09 04:05:06 +0900

    feat : Search ViewController 및 ViewModel 구현 (#194)

---
+++ KCS/KCS/Presentation/Search/ViewModel/SearchViewModelImpl.swift
@@ -0,0 +1,35 @@
+//
+//  SearchViewModelImpl.swift
+//  KCS
+//
+//  Created by 조성민 on 2/8/24.
+//
+
+import RxRelay
+
+final class SearchViewModelImpl: SearchViewModel {
+    
+    var generateDataOutput = PublishRelay<[String]>()
+    
+    func action(input: SearchViewModelInputCase) {
+        switch input {
+        case .textChanged(let text):
+            textChanged(text: text)
+        }
+    }
+    
+}
+
+private extension SearchViewModelImpl {
+    
+    func textChanged(text: String) {
+        if text.isEmpty {
+            // TODO: recentHistory usecase 실행(debounce) 후 generateDataOutput.accept([])
+            generateDataOutput.accept(["recentHistory1", "recentHistory2", "recentHistory3", "recentHistory4"])
+        } else {
+            // TODO: autoCompletion usecase 실행(debounce) 후 generateDataOutput.accept([])
+            generateDataOutput.accept(["completion1", "completion2", "completion3"])
+        }
+    }
+    
+}
