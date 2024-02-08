ff761ac1e14c5c372a23bd22207d314f7530a038
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-02-08 19:39:21 +0900

    fix : alert 선언 방법 수정 (#184)

--- KCS/KCS/Presentation/Extension/UIViewController+Alert.swift
+++ KCS/KCS/Presentation/Extension/UIViewController+Alert.swift
@@ -20,24 +20,21 @@ extension UIViewController {
     }
     
     func presentLocationAlert() {
-        let requestLocationServiceAlert: UIAlertController = {
-            let alertController = UIAlertController(
-                title: "위치 정보 이용",
-                message: "위치 서비스를 사용할 수 없습니다.\n디바이스의 '설정 > 개인정보 보호'에서 위치 서비스를 켜주세요.",
-                preferredStyle: .alert
-            )
-            let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
-                if let appSetting = URL(string: UIApplication.openSettingsURLString) {
-                    UIApplication.shared.open(appSetting)
-                }
+        let requestLocationServiceAlert = UIAlertController(
+            title: "위치 정보 이용",
+            message: "위치 서비스를 사용할 수 없습니다.\n디바이스의 '설정 > 개인정보 보호'에서 위치 서비스를 켜주세요.",
+            preferredStyle: .alert
+        )
+        let goSetting = UIAlertAction(title: "설정으로 이동", style: .destructive) { _ in
+            if let appSetting = URL(string: UIApplication.openSettingsURLString) {
+                UIApplication.shared.open(appSetting)
             }
-            let cancel = UIAlertAction(title: "취소", style: .cancel)
-            
-            alertController.addAction(cancel)
-            alertController.addAction(goSetting)
-            
-            return alertController
-        }()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        
+        requestLocationServiceAlert.addAction(cancel)
+        requestLocationServiceAlert.addAction(goSetting)
+        
         
         if let presentController = presentedViewController {
             presentController.presentLocationAlert()
