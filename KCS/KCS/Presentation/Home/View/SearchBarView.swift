e6b2e513d84cc1825419e63bf4f1a6af1db55a26
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-02-11 02:19:14 +0900

    feat : search bar 구현 (#205)

--- KCS/KCS/Presentation/Home/View/SearchBarView.swift
+++ KCS/KCS/Presentation/Home/View/SearchBarView.swift
@@ -6,23 +6,39 @@
 //
 
 import UIKit
+import RxSwift
 
 final class SearchBarView: UIView {
     
-    private let searchingKeywordLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
+    private let disposeBag = DisposeBag()
+    
+    lazy var searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "가게명, 업종, 주소 검색"
+        textField.returnKeyType = .search
         
-        return label
+        return textField
     }()
     
-    func setSearchKeyword(keyword: String) {
-        searchingKeywordLabel.text = keyword
-    }
+    var searchImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = SystemImage.search
+        imageView.tintColor = .grayLabel
+        
+        return imageView
+    }()
     
-    func getSearchKeyword() -> String? {
-        return searchingKeywordLabel.text
-    }
+    var xmarkImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = SystemImage.xmark
+        imageView.tintColor = .grayLabel
+        imageView.isHidden = true
+        
+        return imageView
+    }()
     
     init() {
         super.init(frame: .zero)
@@ -42,17 +58,59 @@ private extension SearchBarView {
     
     func setup() {
         backgroundColor = .white
+        
+        searchTextField.rx.text
+            .bind { [weak self] text in
+                guard let self = self else { return }
+                if let text = text, !text.isEmpty {
+                    switchToXmark()
+                } else {
+                    switchToSearch()
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        setLayerCorner(cornerRadius: 12)
+        setLayerShadow(shadowOffset: CGSize(width: 0, height: 3))
+        switchToSearch()
     }
     
     func addUIComponents() {
-        addSubview(searchingKeywordLabel)
+        addSubview(searchTextField)
+        addSubview(xmarkImageView)
+        addSubview(searchImageView)
     }
     
     func configureConstraints() {
         NSLayoutConstraint.activate([
-            searchingKeywordLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
-            searchingKeywordLabel.centerXAnchor.constraint(equalTo: centerXAnchor)
+            xmarkImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            xmarkImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            xmarkImageView.widthAnchor.constraint(equalToConstant: 16),
+            xmarkImageView.heightAnchor.constraint(equalToConstant: 16)
         ])
+        
+        NSLayoutConstraint.activate([
+            searchImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            searchImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            searchImageView.widthAnchor.constraint(equalToConstant: 16),
+            searchImageView.heightAnchor.constraint(equalToConstant: 16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            searchTextField.centerYAnchor.constraint(equalTo: centerYAnchor),
+            searchTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            searchTextField.trailingAnchor.constraint(equalTo: xmarkImageView.leadingAnchor, constant: -5)
+        ])
+    }
+    
+    func switchToXmark() {
+            searchImageView.isHidden = true
+            xmarkImageView.isHidden = false
+    }
+    
+    func switchToSearch() {
+        searchImageView.isHidden = false
+        xmarkImageView.isHidden = true
     }
     
 }
