0ad98d7e97225fa7093d9e18b4f00dd4a0b4ffe5
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-02-11 02:42:12 +0900

    chore : 뒤로가기 버튼 이미지 Assets로 추가 (#205)

--- KCS/KCS/Util/SystemImage.swift
+++ KCS/KCS/Util/SystemImage.swift
@@ -12,7 +12,6 @@ enum SystemImage {
     static let circle = UIImage(systemName: "circle.fill")
     static let phone = UIImage(systemName: "phone.fill")
     static let refresh = UIImage(systemName: "arrow.clockwise")
-    static let back = UIImage(systemName: "arrow.backward")
     static let search = UIImage(systemName: "magnifyingglass")
     static let xmark = UIImage(systemName: "xmark")
     
