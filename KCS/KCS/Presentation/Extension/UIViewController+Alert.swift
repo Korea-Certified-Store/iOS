f1be95aafad3fb4b31d0d71a62d0fd641b4b5095
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-02-14 23:14:53 +0900

    feat : Figma에 맞게 Toast 수정 (#2447)

--- KCS/KCS/Presentation/Extension/UIViewController+Alert.swift
+++ KCS/KCS/Presentation/Extension/UIViewController+Alert.swift
@@ -43,44 +43,73 @@ extension UIViewController {
     }
     
     func showToast(message: String) {
-        let toastLabel = UILabel()
-        toastLabel.translatesAutoresizingMaskIntoConstraints = false
-        toastLabel.backgroundColor = .black.withAlphaComponent(0.6)
-        toastLabel.textColor = .white
-        toastLabel.font = .pretendard(size: 14, weight: .medium)
-        toastLabel.textAlignment = .center
-        toastLabel.text = message
-        toastLabel.alpha = 0
-        toastLabel.setLayerCorner(cornerRadius: 12)
-        toastLabel.clipsToBounds = true
-        view.addSubview(toastLabel)
-        NSLayoutConstraint.activate([
-            toastLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            toastLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            toastLabel.widthAnchor.constraint(equalToConstant: 150),
-            toastLabel.heightAnchor.constraint(equalToConstant: 30)
-        ])
+        let toastView = makeToastView(message: message)
+        
+        let windows = UIApplication.shared.connectedScenes
+        let scene = windows.first { $0.activationState == .foregroundActive }
+        if let windowScene = scene as? UIWindowScene, let windowView = windowScene.windows.first {
+            windowView.addSubview(toastView)
+            NSLayoutConstraint.activate([
+                toastView.centerXAnchor.constraint(equalTo: windowView.centerXAnchor),
+                toastView.bottomAnchor.constraint(equalTo: windowView.bottomAnchor, constant: -82)
+            ])
+        }
         
         UIView.animate(
             withDuration: 0.4,
             delay: 0,
             options: .curveEaseIn,
             animations: {
-                toastLabel.alpha = 1.0
+                toastView.alpha = 1.0
             },
             completion: { _ in
                 UIView.animate(
-                    withDuration: 0.8,
-                    delay: 1.4,
+                    withDuration: 0.6,
+                    delay: 2.0,
                     options: .curveEaseOut,
                     animations: {
-                        toastLabel.alpha = 0.0
+                        toastView.alpha = 0.0
                     }, completion: { _ in
-                        toastLabel.removeFromSuperview()
+                        toastView.removeFromSuperview()
                     }
                 )
             }
         )
+    }
+    
+    func makeToastView(message: String) -> UIStackView {
+        let toastImageView = UIImageView(
+            image: SystemImage.toast?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        )
+        toastImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let toastLabel = UILabel()
+        toastLabel.backgroundColor = .clear
+        toastLabel.textColor = .white
+        toastLabel.font = .pretendard(size: 14, weight: .regular)
+        toastLabel.text = message
+        toastLabel.clipsToBounds = true
+        
+        let toastView = UIStackView()
+        toastView.translatesAutoresizingMaskIntoConstraints = false
+        toastView.backgroundColor = .black.withAlphaComponent(0.65)
+        toastView.alpha = 0
+        toastView.setLayerCorner(cornerRadius: 20)
+        toastView.isLayoutMarginsRelativeArrangement = true
+        toastView.layoutMargins = UIEdgeInsets(top: 11, left: 15, bottom: 10, right: 13)
+        toastView.spacing = 8
+        toastView.alignment = .center
+        toastView.distribution = .fill
+        
+        toastView.addArrangedSubview(toastImageView)
+        toastView.addArrangedSubview(toastLabel)
+        
+        NSLayoutConstraint.activate([
+            toastImageView.widthAnchor.constraint(equalToConstant: 16),
+            toastImageView.heightAnchor.constraint(equalToConstant: 16)
+        ])
+        
+        return toastView
     }
     
 }
