b0053fae5b0cdc0373b548ff7d0a953f8fa0da70
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-02-17 18:47:54 +0900

    feat : xmark 시스템 이미지 추가 (#260)

--- KCS/KCS/Util/SystemImage.swift
+++ KCS/KCS/Util/SystemImage.swift
@@ -15,5 +15,6 @@ enum SystemImage {
     static let remove = UIImage(systemName: "xmark.circle.fill")
     static let search = UIImage(systemName: "magnifyingglass")
     static let toast = UIImage(systemName: "exclamationmark.circle")
+    static let clear = UIImage(systemName: "xmark")
     
 }
