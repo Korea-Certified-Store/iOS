695974cc84deebb2022579a9afd623781a59d2de
k2645 <k2645@naver.com>
2024-02-18 16:13:23 +0900

    feat : 주소 layout 추가 (#269)

--- KCS/KCS/Presentation/NewStoreRequest/View/NewStoreRequestViewController.swift
+++ KCS/KCS/Presentation/NewStoreRequest/View/NewStoreRequestViewController.swift
@@ -110,6 +110,85 @@ final class NewStoreRequestViewController: UIViewController {
         return label
     }()
     
+    private let addressHeaderLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "주소"
+        label.font = .pretendard(size: 14, weight: .medium)
+        label.textColor = .kcsGray1
+        
+        return label
+    }()
+    
+    private lazy var addressTextField: NewStoreTextField = {
+        let textField = NewStoreTextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "주소를 검색해 주세요."
+        textField.rx.controlEvent([.editingDidBegin])
+            .asObservable()
+            .bind { [weak self] _ in
+                textField.setSelectedUI()
+                self?.titleWarningLabel.isHidden = true
+            }
+            .disposed(by: disposeBag)
+        
+        return textField
+    }()
+    
+    private lazy var searchAddressButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("주소 검색", for: .normal)
+        button.titleLabel?.font = UIFont.pretendard(size: 15, weight: .medium)
+        button.titleLabel?.textColor = .white
+        button.backgroundColor = .placeholderText
+        button.layer.borderColor = UIColor.kcsGray1.cgColor
+        button.layer.borderWidth = 0.7
+        button.setLayerCorner(cornerRadius: 10)
+        button.rx.tap
+            .bind { [weak self] in
+                let addressViewController = AddressViewController(nibName: nil, bundle: nil)
+                addressViewController.modalPresentationStyle = .fullScreen
+                self?.present(addressViewController, animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        return button
+    }()
+    
+    private lazy var detailAddressTextField: NewStoreTextField = {
+        let textField = NewStoreTextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "상세 주소를 입력해 주세요."
+        textField.rx.controlEvent([.editingDidBegin])
+            .asObservable()
+            .bind { [weak self] _ in
+                textField.setSelectedUI()
+                self?.titleWarningLabel.isHidden = true
+            }
+            .disposed(by: disposeBag)
+        
+        textField.rx.controlEvent([.editingDidEnd])
+            .asObservable()
+            .bind { [weak self] _ in
+                self?.viewModel.action(input: .titleEditEnd(text: textField.text ?? ""))
+            }
+            .disposed(by: disposeBag)
+        
+        return textField
+    }()
+    
+    private let addressWarningLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "주소를 입력해 주세요."
+        label.font = .pretendard(size: 12, weight: .regular)
+        label.textColor = .uiTextFieldWarning
+        label.isHidden = true
+        
+        return label
+    }()
+    
     private let viewModel: NewStoreRequestViewModel
     
     init(viewModel: NewStoreRequestViewModel) {
@@ -142,6 +221,11 @@ private extension NewStoreRequestViewController {
         scrollContentView.addSubview(titleHeaderLabel)
         scrollContentView.addSubview(titleTextField)
         scrollContentView.addSubview(titleWarningLabel)
+        scrollContentView.addSubview(addressHeaderLabel)
+        scrollContentView.addSubview(addressTextField)
+        scrollContentView.addSubview(searchAddressButton)
+        scrollContentView.addSubview(detailAddressTextField)
+        scrollContentView.addSubview(addressWarningLabel)
     }
     
     func configureConstraints() {
@@ -168,7 +252,7 @@ private extension NewStoreRequestViewController {
         ])
         
         NSLayoutConstraint.activate([
-            titleHeaderLabel.topAnchor.constraint(equalTo: scrollContentView.topAnchor, constant: 0),
+            titleHeaderLabel.topAnchor.constraint(equalTo: scrollContentView.topAnchor, constant: 25),
             titleHeaderLabel.leadingAnchor.constraint(equalTo: scrollContentView.leadingAnchor, constant: 16)
         ])
         
@@ -182,6 +266,37 @@ private extension NewStoreRequestViewController {
         NSLayoutConstraint.activate([
             titleWarningLabel.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 6),
             titleWarningLabel.leadingAnchor.constraint(equalTo: titleTextField.leadingAnchor, constant: 16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            addressHeaderLabel.topAnchor.constraint(equalTo: titleTextField.bottomAnchor, constant: 48),
+            addressHeaderLabel.leadingAnchor.constraint(equalTo: scrollContentView.leadingAnchor, constant: 16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            addressTextField.topAnchor.constraint(equalTo: addressHeaderLabel.bottomAnchor, constant: 8),
+            addressTextField.leadingAnchor.constraint(equalTo: scrollContentView.leadingAnchor, constant: 16),
+            addressTextField.trailingAnchor.constraint(equalTo: scrollContentView.trailingAnchor, constant: -113),
+            addressTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        NSLayoutConstraint.activate([
+            searchAddressButton.centerYAnchor.constraint(equalTo: addressTextField.centerYAnchor),
+            searchAddressButton.leadingAnchor.constraint(equalTo: addressTextField.trailingAnchor, constant: 9),
+            searchAddressButton.trailingAnchor.constraint(equalTo: scrollContentView.trailingAnchor, constant: -16),
+            searchAddressButton.heightAnchor.constraint(equalToConstant: 42)
+        ])
+        
+        NSLayoutConstraint.activate([
+            detailAddressTextField.topAnchor.constraint(equalTo: addressTextField.bottomAnchor, constant: 12),
+            detailAddressTextField.leadingAnchor.constraint(equalTo: scrollContentView.leadingAnchor, constant: 16),
+            detailAddressTextField.trailingAnchor.constraint(equalTo: scrollContentView.trailingAnchor, constant: -16),
+            detailAddressTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        NSLayoutConstraint.activate([
+            addressWarningLabel.topAnchor.constraint(equalTo: detailAddressTextField.bottomAnchor, constant: 6),
+            addressWarningLabel.leadingAnchor.constraint(equalTo: detailAddressTextField.leadingAnchor, constant: 16)
         ])
     }
     
