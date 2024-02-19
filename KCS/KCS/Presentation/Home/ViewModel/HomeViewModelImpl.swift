36fa8dda6a479d5241215978b69afea8aa5732b4
k2645 <k2645@naver.com>
2024-02-20 03:49:03 +0900

    chore : TODO 추가 (#286)

--- KCS/KCS/Presentation/Home/ViewModel/HomeViewModelImpl.swift
+++ KCS/KCS/Presentation/Home/ViewModel/HomeViewModelImpl.swift
@@ -32,6 +32,7 @@ final class HomeViewModelImpl: HomeViewModel {
     let moreStoreButtonHiddenOutput = PublishRelay<Void>()
     
     private let disposeBag = DisposeBag()
+    // TODO: activatedFilter 수정 필요
     private var activatedFilter: [CertificationType] = []
     private var fetchCount: Int = 1
     private var maxFetchCount: Int = 1
