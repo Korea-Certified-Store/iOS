ce1aee7ff307157b150fd80c2d8f4a69f2b7cdea
k2645 <k2645@naver.com>
2024-02-29 01:27:39 +0900

    test : FetchImageRepository 에러 코드 제외한 테스트 코드 작성 (#345)

---
+++ KCS/KCSUnitTest/RepositoryTest/FetchImageRepositoryTests.swift
@@ -0,0 +1,147 @@
+//
+//  FetchImageRepositoryTests.swift
+//  KCSUnitTest
+//
+//  Created by 김영현 on 2/26/24.
+//
+
+import XCTest
+@testable import KCS
+import Alamofire
+import RxSwift
+import RxBlocking
+
+final class FetchImageRepositoryTests: XCTestCase {
+    
+    enum URLString: String {
+        case cache = "test_cache_image_URL"
+        case noCache = "test_no_cache_image_URL"
+        case fail = "url"
+        case wrongURL = ""
+    }
+    
+    private var fetchImageRepository: FetchImageRepository!
+    private var imageCache: ImageCache!
+    private var mockImage: MockImage!
+    private var session: Session!
+    private let imageString = "MockImage"
+
+    override func setUp() {
+        imageCache = ImageCache(cache: NSCache<NSURL, NSData>())
+        mockImage = MockImage()
+        session = {
+            let configuration: URLSessionConfiguration = {
+                let configuration = URLSessionConfiguration.default
+                configuration.protocolClasses = [MockURLProtocol.self]
+                return configuration
+            }()
+            return Session(configuration: configuration)
+        }()
+        fetchImageRepository = FetchImageRepositoryImpl(
+            cache: imageCache,
+            session: session
+        )
+    }
+
+    func test_캐시데이터에_이미지가_존재하고_통신에_성공한_경우() throws {
+        // Given
+        MockURLProtocol.responseWithStatusCode(code: 200)
+        MockURLProtocol.setResponseFile(type: .fetchImageFile)
+        let urlString = URLString.cache.rawValue
+        let imageData = mockImage.getImageURL(imageString: imageString)
+        guard let url = NSURL(string: urlString) else {
+            XCTFail("데이터 변환 실패")
+            return
+        }
+        imageCache.setImageData(imageData as NSData, for: url)
+        fetchImageRepository = FetchImageRepositoryImpl(
+            cache: imageCache,
+            session: session
+        )
+        
+        // When
+        let imageObservable = fetchImageRepository.fetchImage(url: urlString).toBlocking()
+        
+        // Then
+        do {
+            guard let result = try imageObservable.first() else {
+                XCTFail("BlockingObservable 값 추출 실패")
+                return
+            }
+            XCTAssertEqual(Data(imageData), result)
+        } catch {
+            XCTFail("캐시데이터 이미지 fetch 실패")
+        }
+    }
+    
+    func test_캐시데이터에_이미지가_존재하지_않고_통신에_성공한_경우() throws {
+        // Given
+        MockURLProtocol.responseWithStatusCode(code: 200)
+        MockURLProtocol.setResponseFile(type: .fetchImageFile)
+        let urlString = URLString.noCache.rawValue
+        let imageData = mockImage.getImageURL(imageString: imageString)
+        fetchImageRepository = FetchImageRepositoryImpl(
+            cache: imageCache,
+            session: session
+        )
+        
+        // When
+        let imageObservable = fetchImageRepository.fetchImage(url: urlString).toBlocking()
+        
+        // Then
+        do {
+            guard let result = try imageObservable.first() else {
+                XCTFail("BlockingObservable 값 추출 실패")
+                return
+            }
+            XCTAssertEqual(Data(imageData), result)
+        } catch {
+            XCTFail("캐시데이터 이미지 fetch 실패")
+        }
+    }
+    
+    func test_URL에_맞는_이미지_데이터가_존재하지_않는_경우() throws {
+        // Given
+        MockURLProtocol.responseWithStatusCode(code: 200)
+        MockURLProtocol.setResponseFile(type: .fetchImageFileFail)
+        let urlString = URLString.fail.rawValue
+        fetchImageRepository = FetchImageRepositoryImpl(
+            cache: imageCache,
+            session: session
+        )
+        
+        // When
+        let imageObservable = fetchImageRepository.fetchImage(url: urlString).toBlocking().materialize()
+        
+        // Then
+        switch imageObservable {
+        case .completed(_):
+            XCTFail("ImageRepositoryError 에러를 담은 데이터를 반환 실패")
+        case .failed(_, let error):
+            XCTAssertEqual(error as? ImageRepositoryError, ImageRepositoryError.noImageData)
+        }
+    }
+    
+    func test_잘못된_URL을_요청했을_경우() throws {
+        // Given
+        MockURLProtocol.responseWithStatusCode(code: 200)
+        MockURLProtocol.setResponseFile(type: .fetchImageFileFail)
+        let urlString = URLString.wrongURL.rawValue
+        fetchImageRepository = FetchImageRepositoryImpl(
+            cache: imageCache,
+            session: session
+        )
+        
+        // When
+        let imageObservable = fetchImageRepository.fetchImage(url: urlString).toBlocking().materialize()
+        
+        // Then
+        switch imageObservable {
+        case .completed(_):
+            XCTFail("ErrorAlertMessage.client 에러를 담은 데이터를 반환 실패")
+        case .failed(_, let error):
+            XCTAssertEqual(error as? ErrorAlertMessage, ErrorAlertMessage.client)
+        }
+    }
+    
+}
