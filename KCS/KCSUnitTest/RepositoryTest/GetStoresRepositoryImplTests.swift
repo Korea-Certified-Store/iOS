e1b197755146161e92fe6a3be3aa74ebe9eed7b6
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-02-29 03:15:13 +0900

    test : GetStoreRepositoryImpl 테스트 (#348)

---
+++ KCS/KCSUnitTest/RepositoryTest/GetStoresRepositoryImplTests.swift
@@ -0,0 +1,67 @@
+//
+//  GetStoresRepositoryImplTests.swift
+//  KCSUnitTest
+//
+//  Created by 조성민 on 2/29/24.
+//
+
+import XCTest
+@testable import KCS
+import RxSwift
+
+final class GetStoresRepositoryImplTestsEntity {
+    
+    let emptyStoreArray: [Store] = []
+    var stores: [Store] = []
+    
+    init() {
+        self.convertToDTO()
+    }
+    
+    func convertToDTO() {
+        guard let path = Bundle(for: type(of: self)).url(forResource: "FetchStoresSuccessWithManyStores", withExtension: "json") else {
+            return
+        }
+        guard let jsonString = try? String(contentsOf: path) else {
+            return
+        }
+        let decoder = JSONDecoder()
+        let data = jsonString.data(using: .utf8)
+        if let data = data,
+           let response = try? decoder.decode(RefreshStoreResponse.self, from: data),
+           let storesArray = try? response.data.map({ try $0.map { try $0.toEntity() } }) {
+            self.stores = storesArray.flatMap( { $0 } )
+        } else {
+            return
+        }
+    }
+    
+}
+
+final class GetStoresRepositoryImplTests: XCTestCase {
+
+    private var getStoresRepository: GetStoresRepository!
+    private var storeStorage: StoreStorage!
+    private var disposeBag: DisposeBag!
+    private var testEntity: GetStoresRepositoryImplTestsEntity!
+    
+    override func setUp() {
+        storeStorage = StoreStorage()
+        getStoresRepository = GetStoresRepositoryImpl(storeStorage: storeStorage)
+        disposeBag = DisposeBag()
+        testEntity = GetStoresRepositoryImplTestsEntity()
+    }
+
+    func test_Store가_0개인_경우() {
+        storeStorage.stores = testEntity.emptyStoreArray
+        
+        XCTAssertEqual(getStoresRepository.getStores(), testEntity.emptyStoreArray)
+    }
+    
+    func test_Store가_1개_이상인_경우() {
+        storeStorage.stores = testEntity.stores
+        
+        XCTAssertEqual(getStoresRepository.getStores(), testEntity.stores)
+    }
+
+}
