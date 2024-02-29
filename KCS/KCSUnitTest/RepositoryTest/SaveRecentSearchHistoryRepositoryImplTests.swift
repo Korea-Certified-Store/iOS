443f09243f13ca1e33d685fd11cfa9cdbed27038
k2645 <k2645@naver.com>
2024-03-01 01:49:34 +0900

    refactor : 결과값도 constant에 추가 (#354)

--- KCS/KCSUnitTest/RepositoryTest/SaveRecentSearchHistoryRepositoryImplTests.swift
+++ KCS/KCSUnitTest/RepositoryTest/SaveRecentSearchHistoryRepositoryImplTests.swift
@@ -20,6 +20,17 @@ struct SaveRecentSearchHistoryRepositoryImplTestsConstant {
         "검색어21", "검색어22", "검색어23", "검색어24", "검색어25",
         "검색어26", "검색어27", "검색어28", "검색어29", "검색어30"
     ]
+    let resultOneRecentHistory: [String] = ["검색어1"]
+    let resultNewRecentHistory: [String] = ["검색어4", "검색어1", "검색어2", "검색어3"]
+    let resultReinsertRecentHistory: [String] = ["검색어3", "검색어1", "검색어2"]
+    let resultOver30RecentHistory: [String] = [
+        "검색어31", "검색어1", "검색어2", "검색어3", "검색어4",
+        "검색어5", "검색어6", "검색어7", "검색어8", "검색어9",
+        "검색어10", "검색어11", "검색어12", "검색어13", "검색어14",
+        "검색어15", "검색어16", "검색어17", "검색어18", "검색어19",
+        "검색어20", "검색어21", "검색어22", "검색어23", "검색어24",
+        "검색어25", "검색어26", "검색어27", "검색어28", "검색어29"
+    ]
     
 }
 
@@ -48,7 +59,7 @@ final class SaveRecentSearchHistoryRepositoryImplTests: XCTestCase {
         
         // Then
         if let recentHistory = userDefaults.array(forKey: constant.key) as? [String] {
-            XCTAssertEqual(recentHistory, ["검색어1"])
+            XCTAssertEqual(recentHistory, constant.resultOneRecentHistory)
         } else {
             XCTFail("최근 검색어 추가 실패")
         }
@@ -66,7 +77,7 @@ final class SaveRecentSearchHistoryRepositoryImplTests: XCTestCase {
         
         // Then
         if let recentHistory = userDefaults.array(forKey: constant.key) as? [String] {
-            XCTAssertEqual(recentHistory, ["검색어4", "검색어1", "검색어2", "검색어3"])
+            XCTAssertEqual(recentHistory, constant.resultNewRecentHistory)
         } else {
             XCTFail("새로운 최근 검색어 추가 실패")
         }
@@ -84,7 +95,7 @@ final class SaveRecentSearchHistoryRepositoryImplTests: XCTestCase {
         
         // Then
         if let recentHistory = userDefaults.array(forKey: constant.key) as? [String] {
-            XCTAssertEqual(recentHistory, ["검색어3", "검색어1", "검색어2"])
+            XCTAssertEqual(recentHistory, constant.resultReinsertRecentHistory)
         } else {
             XCTFail("존재하던 최근 검색어 추가 실패")
         }
@@ -102,14 +113,7 @@ final class SaveRecentSearchHistoryRepositoryImplTests: XCTestCase {
         
         // Then
         if let recentHistory = userDefaults.array(forKey: constant.key) as? [String] {
-            XCTAssertEqual(recentHistory, [
-                "검색어31", "검색어1", "검색어2", "검색어3", "검색어4",
-                "검색어5", "검색어6", "검색어7", "검색어8", "검색어9",
-                "검색어10", "검색어11", "검색어12", "검색어13", "검색어14",
-                "검색어15", "검색어16", "검색어17", "검색어18", "검색어19",
-                "검색어20", "검색어21", "검색어22", "검색어23", "검색어24",
-                "검색어25", "검색어26", "검색어27", "검색어28", "검색어29"
-            ])
+            XCTAssertEqual(recentHistory, constant.resultOver30RecentHistory)
         } else {
             XCTFail("30개 초과시 새로운 최근 검색어 추가 실패")
         }
