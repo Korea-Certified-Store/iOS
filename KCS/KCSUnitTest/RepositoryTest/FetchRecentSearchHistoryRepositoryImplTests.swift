f0163cc87b6ca89e3c0830c54b7ff328fc01e5b9
k2645 <k2645@naver.com>
2024-02-29 17:49:16 +0900

    test : FetchRecentSearchHistoryRepositoryImplTests 테스트 코드 작성 (#352)

---
+++ KCS/KCSUnitTest/RepositoryTest/FetchRecentSearchHistoryRepositoryImplTests.swift
@@ -0,0 +1,67 @@
+//
+//  FetchRecentSearchHistoryRepositoryImplTests.swift
+//  KCSUnitTest
+//
+//  Created by 김영현 on 2/29/24.
+//
+
+import XCTest
+@testable import KCS
+import RxSwift
+import RxBlocking
+
+final class FetchRecentSearchHistoryRepositoryImplTestsEntity {
+    
+    let emptyRecentHistory: [String] = []
+    let RecentHistory: [String] = ["검색어1", "검색어2", "검색어3"]
+    
+}
+
+final class FetchRecentSearchHistoryRepositoryImplTests: XCTestCase {
+    
+    private var fetchRecentSearchHistoryRepository: FetchRecentSearchHistoryRepository!
+    private var userDefaults: MockUserDefaults!
+    private var fetchRecentSearchHistoryRepositoryImplTestsEntity: FetchRecentSearchHistoryRepositoryImplTestsEntity!
+
+    override func setUp() {
+        userDefaults = MockUserDefaults()
+        fetchRecentSearchHistoryRepositoryImplTestsEntity = FetchRecentSearchHistoryRepositoryImplTestsEntity()
+        fetchRecentSearchHistoryRepository = FetchRecentSearchHistoryRepositoryImpl(
+            userDefaults: userDefaults
+        )
+    }
+    
+    func test_userDefaults에서_빈_배열값을_가져오는_경우() {
+        // Given
+        let key = "recentSearchKeywords"
+        
+        // When
+        let result = fetchRecentSearchHistoryRepository.fetchRecentSearchHistory().toBlocking()
+        
+        // Then
+        do {
+            let resultArray = try result.first()
+            XCTAssertEqual(resultArray, fetchRecentSearchHistoryRepositoryImplTestsEntity.emptyRecentHistory)
+        } catch {
+            XCTFail("빈 배열 불러오기 실패")
+        }
+    }
+    
+    func test_userDefaults에서_문자열_배열값을_가져오는_경우() {
+        // Given
+        let key = "recentSearchKeywords"
+        userDefaults.set(fetchRecentSearchHistoryRepositoryImplTestsEntity.RecentHistory, forKey: key)
+        
+        // When
+        let result = fetchRecentSearchHistoryRepository.fetchRecentSearchHistory().toBlocking()
+        
+        // Then
+        do {
+            let resultArray = try result.first()
+            XCTAssertEqual(resultArray, userDefaults.array(forKey: key) as? [String])
+        } catch {
+            XCTFail("문자열 배열 불러오기 실패")
+        }
+    }
+
+}
