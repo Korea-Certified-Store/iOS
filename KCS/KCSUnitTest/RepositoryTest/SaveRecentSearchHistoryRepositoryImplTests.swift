99c7d69aea4b16654a99b1739e174f6ccebab980
k2645 <k2645@naver.com>
2024-02-29 23:33:54 +0900

    test : SaveRecentSearchHistoryRepositoryImpl 테스트 코드 작성 (#354)

---
+++ KCS/KCSUnitTest/RepositoryTest/SaveRecentSearchHistoryRepositoryImplTests.swift
@@ -0,0 +1,118 @@
+//
+//  SaveRecentSearchHistoryRepositoryImplTests.swift
+//  KCSUnitTest
+//
+//  Created by 김영현 on 2/29/24.
+//
+
+import XCTest
+@testable import KCS
+
+struct SaveRecentSearchHistoryRepositoryImplTestsConstant {
+    
+    let key = "recentSearchKeywords"
+    let recentHistory: [String] = ["검색어1", "검색어2", "검색어3"]
+    let recentHistoryOver30: [String] = [
+        "검색어1", "검색어2", "검색어3", "검색어4", "검색어5",
+        "검색어6", "검색어7", "검색어8", "검색어9", "검색어10",
+        "검색어11", "검색어12", "검색어13", "검색어14", "검색어15",
+        "검색어16", "검색어17", "검색어18", "검색어19", "검색어20",
+        "검색어21", "검색어22", "검색어23", "검색어24", "검색어25",
+        "검색어26", "검색어27", "검색어28", "검색어29", "검색어30"
+    ]
+    
+}
+
+final class SaveRecentSearchHistoryRepositoryImplTests: XCTestCase {
+    
+    private var saveRecentSearchHistoryRepository: SaveRecentSearchHistoryRepository!
+    private var userDefaults: MockUserDefaults!
+    private var constant: SaveRecentSearchHistoryRepositoryImplTestsConstant!
+
+    override func setUp() {
+        userDefaults = MockUserDefaults()
+        constant = SaveRecentSearchHistoryRepositoryImplTestsConstant()
+        saveRecentSearchHistoryRepository = SaveRecentSearchHistoryRepositoryImpl(
+            userDefaults: userDefaults
+        )
+    }
+    
+    func test_최근검색어가_처음_추가되는_경우() {
+        // Given
+        let recentSearchKeyword = "검색어1"
+        
+        // When
+        saveRecentSearchHistoryRepository.saveRecentSearchHistory(
+            recentSearchKeyword: recentSearchKeyword
+        )
+        
+        // Then
+        if let recentHistory = userDefaults.array(forKey: constant.key) as? [String] {
+            XCTAssertEqual(recentHistory, ["검색어1"])
+        } else {
+            XCTFail("최근 검색어 추가 실패")
+        }
+    }
+
+    func test_새로운_최근검색어가_추가되는_경우() {
+        // Given
+        let recentSearchKeyword = "검색어4"
+        userDefaults.set(constant.recentHistory, forKey: constant.key)
+        
+        // When
+        saveRecentSearchHistoryRepository.saveRecentSearchHistory(
+            recentSearchKeyword: recentSearchKeyword
+        )
+        
+        // Then
+        if let recentHistory = userDefaults.array(forKey: constant.key) as? [String] {
+            XCTAssertEqual(recentHistory, ["검색어4", "검색어1", "검색어2", "검색어3"])
+        } else {
+            XCTFail("새로운 최근 검색어 추가 실패")
+        }
+    }
+    
+    func test_최근검색어_배열에_존재하던_검색어가_다시_저장되는_경우() {
+        // Given
+        let recentSearchKeyword = "검색어3"
+        userDefaults.set(constant.recentHistory, forKey: constant.key)
+        
+        // When
+        saveRecentSearchHistoryRepository.saveRecentSearchHistory(
+            recentSearchKeyword: recentSearchKeyword
+        )
+        
+        // Then
+        if let recentHistory = userDefaults.array(forKey: constant.key) as? [String] {
+            XCTAssertEqual(recentHistory, ["검색어3", "검색어1", "검색어2"])
+        } else {
+            XCTFail("존재하던 최근 검색어 추가 실패")
+        }
+    }
+    
+    func test_최근검색어_배열이_30개가_넘어가는_경우() {
+        // Given
+        let recentSearchKeyword = "검색어31"
+        userDefaults.set(constant.recentHistoryOver30, forKey: constant.key)
+        
+        // When
+        saveRecentSearchHistoryRepository.saveRecentSearchHistory(
+            recentSearchKeyword: recentSearchKeyword
+        )
+        
+        // Then
+        if let recentHistory = userDefaults.array(forKey: constant.key) as? [String] {
+            XCTAssertEqual(recentHistory, [
+                "검색어31", "검색어1", "검색어2", "검색어3", "검색어4",
+                "검색어5", "검색어6", "검색어7", "검색어8", "검색어9",
+                "검색어10", "검색어11", "검색어12", "검색어13", "검색어14",
+                "검색어15", "검색어16", "검색어17", "검색어18", "검색어19",
+                "검색어20", "검색어21", "검색어22", "검색어23", "검색어24",
+                "검색어25", "검색어26", "검색어27", "검색어28", "검색어29"
+            ])
+        } else {
+            XCTFail("30개 초과시 새로운 최근 검색어 추가 실패")
+        }
+    }
+
+}
