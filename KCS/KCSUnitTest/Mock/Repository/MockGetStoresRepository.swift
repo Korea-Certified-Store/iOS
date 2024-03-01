6322999c096634b2f10781838b5ae1d821737d3a
k2645 <k2645@naver.com>
2024-03-01 17:37:26 +0900

    test : GetRefreshStoresUseCaseImpl 테스트 코드 작성 (#369)

---
+++ KCS/KCSUnitTest/Mock/Repository/MockGetStoresRepository.swift
@@ -0,0 +1,19 @@
+//
+//  MockGetStoresRepository.swift
+//  KCSUnitTest
+//
+//  Created by 김영현 on 3/1/24.
+//
+
+import Foundation
+@testable import KCS
+
+struct MockGetStoresRepository: GetStoresRepository {
+    
+    var storeStorage: StoreStorage
+    
+    func getStores() -> [Store] {
+        return storeStorage.stores
+    }
+    
+}
