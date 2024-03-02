bb95c1ad29158eec8e559d359a07f0d3a0f20a6c
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-03-02 22:31:52 +0900

    test : SaveRecentSearchHistoryRepositoryImpl 테스트 (#376)

---
+++ KCS/KCSUnitTest/UseCaseTest/SaveRecentSearchHistoryUseCaseImplTests.swift
@@ -0,0 +1,41 @@
+//
+//  SaveRecentSearchHistoryUseCaseImplTests.swift
+//  KCSUnitTest
+//
+//  Created by 조성민 on 3/2/24.
+//
+
+import XCTest
+@testable import KCS
+
+struct SaveRecentSearchHistoryUseCaseImplTestsConstant {
+    
+    let testKeyword = "TestKeyword"
+    let dummyUserDefaults = UserDefaults.standard
+    let resultExecuteCount = 1
+    
+}
+
+final class SaveRecentSearchHistoryUseCaseImplTests: XCTestCase {
+
+    private var saveRecentSearchHistoryUseCase: SaveRecentSearchHistoryUseCaseImpl!
+    private var spySaveRecentSearchHistoryRepository: SpySaveRecentSearchHistoryRepository!
+    private var constant: SaveRecentSearchHistoryUseCaseImplTestsConstant!
+    
+    override func setUp() {
+        constant = SaveRecentSearchHistoryUseCaseImplTestsConstant()
+        spySaveRecentSearchHistoryRepository = SpySaveRecentSearchHistoryRepository(userDefaults: constant.dummyUserDefaults)
+        saveRecentSearchHistoryUseCase = SaveRecentSearchHistoryUseCaseImpl(repository: spySaveRecentSearchHistoryRepository)
+    }
+
+    func test_Repository의_함수를_성공하는_경우() {
+        // Given initial statie
+        
+        // When
+        saveRecentSearchHistoryUseCase.execute(recentSearchKeyword: constant.testKeyword)
+        
+        // Then
+        XCTAssertEqual(spySaveRecentSearchHistoryRepository.executeCount, constant.resultExecuteCount)
+    }
+
+}
