6ed47a0e201c568874cbec0185930795b1d94d31
k2645 <k2645@naver.com>
2024-03-06 00:43:15 +0900

    refactor : input case 수정 (#387)

---
+++ KCS/KCS/Domain/Entity/NewStoreRequestInputCase.swift
@@ -0,0 +1,16 @@
+//
+//  NewStoreRequestInputCase.swift
+//  KCS
+//
+//  Created by 김영현 on 3/5/24.
+//
+
+import Foundation
+
+enum InputCase {
+    
+    case title
+    case address
+    case detailAddress
+    
+}
